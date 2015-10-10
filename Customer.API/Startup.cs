21503d65874f6068b0f1fa36ceb259beb7792887
Chris Berthold <cberthold@technodotnet.com>
2015-10-10 01:31:06 -0400

    add customer api

---
+++ Customer.API/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Customer.API.Startup))]
+
+namespace Customer.API
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
