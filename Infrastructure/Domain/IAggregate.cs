decc62c7af7f5edd3f13ae7144a5ac1583ce1340
Chris Berthold <cberthold@technodotnet.com>
2015-10-22 20:50:01 -0400

    add base abstract implementation of domain repository and add linking to apply events to aggregate interface

--- Infrastructure/Domain/IAggregate.cs
+++ Infrastructure/Domain/IAggregate.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Infrastructure.Events;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace Infrastructure.Domain
 {
     public interface IAggregate
     {
+        IEnumerable<IEvent> UncommitedEvents();
+        void ClearUncommitedEvents();
+        int Version { get; }
+        Guid Id { get; }
+        void ApplyEvent(IEvent @event);
     }
 }
