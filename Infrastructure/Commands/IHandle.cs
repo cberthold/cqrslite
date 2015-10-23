a9e419843bf9264742536fea8d06d1b10e5fa519
Chris Berthold <cberthold@technodotnet.com>
2015-10-22 20:25:56 -0400

    Add domain aggregate base and interfaces for aggregate and interface for command handler

---
+++ Infrastructure/Commands/IHandle.cs
@@ -0,0 +1,15 @@
+﻿using Infrastructure.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Commands
+{
+    public interface IHandle<in TCommand>
+        where TCommand : ICommand
+    {
+        IAggregate Handle(TCommand command);
+    }
+}
