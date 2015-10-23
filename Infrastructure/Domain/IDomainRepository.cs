f42a35e248dff1f9c69b8873be546ee13f83b089
Chris Berthold <cberthold@technodotnet.com>
2015-10-22 20:28:45 -0400

    add interface for the domain repository

---
+++ Infrastructure/Domain/IDomainRepository.cs
@@ -0,0 +1,15 @@
+﻿using Infrastructure.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Domain
+{
+    public interface IDomainRepository
+    {
+        IEnumerable<IEvent> Save<TAggregate>(TAggregate aggregate) where TAggregate : IAggregate;
+        TResult GetById<TResult>(Guid id) where TResult : IAggregate, new();
+    }
+}
