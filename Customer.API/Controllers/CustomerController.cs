fdc5dda613965c2c9b458f159b20864513a2614a
Chris Berthold <cberthold@technodotnet.com>
2015-10-27 21:08:40 -0400

    add customer controller for angular to call

---
+++ Customer.API/Controllers/CustomerController.cs
@@ -0,0 +1,42 @@
+﻿using Infrastructure.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Customer.API.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        private static readonly InMemoryDomainRespository repository = new InMemoryDomainRespository();
+
+        // GET: api/Customer
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Customer/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Customer
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Customer/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Customer/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
