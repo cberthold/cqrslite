74d0e116b4e99b8debe45bb552f37d7fb64d8aeb
Chris Berthold <cberthold@technodotnet.com>
2015-12-11 20:02:39 -0500

    configure additional methods for calling web api

--- UI/app/customers/scripts/factory/customerResource.js
+++ UI/app/customers/scripts/factory/customerResource.js
@@ -10,8 +10,18 @@
 angular.module('customersModuleProviders')
     .factory('CustomerResource', ['CustomerApiConfig', function (CustomerApiConfig) {
 
-        var endpointResource = CustomerApiConfig.getResource('customer');
+        var getUrl = CustomerApiConfig.getEndpointAddress('customer');
+        var listUrl = CustomerApiConfig.getEndpointAddressNameOnlyUri('customer');
 
+        var paramDefaults = null;
+        var actions = {
+            'list': { method: 'GET', isArray: true, url: listUrl },
+            'get': { method: 'GET', url: getUrl },
+            'update': { method: 'PUT', url: getUrl },
+            'delete': { method: 'DELETE', url: getUrl }
+        };
+
+        var endpointResource = CustomerApiConfig.getResource('customer', paramDefaults, actions);
 
         return endpointResource;
     }]);
