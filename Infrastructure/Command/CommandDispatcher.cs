2c4d786db48b69668a493997fa2fffbe86c45469
Chris Berthold <cberthold@technodotnet.com>
2015-12-14 22:14:13 -0500

    add command validation to command dispatcher using a validationfactory that integrates with Autofac - need to find autofac equivalent without using the service locator anti-pattern

--- Infrastructure/Command/CommandDispatcher.cs
+++ Infrastructure/Command/CommandDispatcher.cs
@@ -1,25 +1,56 @@
-﻿using CQRSlite.Commands;
+﻿using Autofac;
+using CQRSlite.Commands;
+using Infrastructure.Exceptions;
+using Infrastructure.Validation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ValidationResult = System.ComponentModel.DataAnnotations.ValidationResult;
+
 
 namespace Infrastructure.Command
 {
     public class CommandDispatcher
     {
         ICommandSender bus;
+        IValidationFactory validationFactory;
 
-        public CommandDispatcher(ICommandSender bus)
+        public CommandDispatcher(ICommandSender bus, IValidationFactory validationFactory)
         {
             this.bus = bus;
+            this.validationFactory = validationFactory;
         }
 
         public void Send<T>(T command) 
             where T : ICommand
         {
+            // validate command - throws exception if not valid
+            ValidateCommand(command);
+            // send command to bus if it validates
             bus.Send(command);
+        }
+
+        private void ValidateCommand<T>(T command)
+        {
+            var results = new List<ValidationResult>();
+
+            foreach (var validator in validationFactory.GetValidators<T>())
+            {
+                var result = validator.Validate(command);
+
+                if (!result.IsValid)
+                {
+                    results.AddRange(result.ToValidationResult());
+                }
+            }
+
+            if(results.Count > 0)
+            {
+                throw new ValidationException(results);
+            }
+            
         }
     }
 }
---
+++ Infrastructure/Validation/ValidationFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using Autofac.Core;
+using Autofac;
+
+namespace Infrastructure.Validation
+{
+    public class ValidationFactory : IValidationFactory
+    {
+        IComponentContext container;
+
+        public ValidationFactory(IComponentContext container)
+        {
+            this.container = container;
+        }
+
+        public IEnumerable<IValidator<T>> GetValidators<T>()
+        {
+            var validators = container.Resolve< IEnumerable<IValidator<T>>>();
+            return validators;
+        }
+    }
+}
