66900c68750660673be603b52950484586873b1d
Chris Berthold <cberthold@technodotnet.com>
2015-12-14 22:19:32 -0500

    add Global Exception Filter to standardize ValidationException output from the API

--- Customer.API/App_Start/WebApiConfig.cs
+++ Customer.API/App_Start/WebApiConfig.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
+using Customer.API.Filters;
 
 namespace Customer.API
 {
@@ -16,6 +17,7 @@ namespace Customer.API
             // Configure Web API to use only bearer token authentication.
             //config.SuppressDefaultHostAuthentication();
             //config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+            config.Filters.Add(new GlobalExceptionAttribute());
 
             // Web API routes
             config.MapHttpAttributeRoutes();
