3fdd91d32c96d433578c3fd466273e2edfde7c3b
Chris Berthold <cberthold@technodotnet.com>
2016-02-18 07:23:17 -0500

    add web api for security bounded context

---
+++ Security.API/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace Security.API.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ Security.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Security.API.WebApiApplication" Language="C#" %>
