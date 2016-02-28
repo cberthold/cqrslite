8d5d8e1086743b12ef1557c8753b06f9af448747
Chris Berthold <cberthold@technodotnet.com>
2016-02-28 13:42:05 -0500

    add signlar project for sending events to UI

---
+++ SignalR.Web/App_Start/Startup.SignalR.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+using SignalR.Web.Providers;
+using SignalR.Web.Models;
+
+namespace SignalR.Web
+{
+    public partial class Startup
+    {
+        public void ConfigureSignalR(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
---
+++ SignalR.Web/Models/NotificationHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Infrastructure.Events;
+
+namespace SignalR.Web.Models
+{
+    public class NotificationHub : Hub
+    {
+        public void PublishEvents(params Infrastructure.Events.IEvent[] @events)
+        {
+            Clients.All.hello();
+        }
+    }
+}
