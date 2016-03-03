ef9b3f4ffdcad7e24a103ea03778adc9e049814c
Chris Berthold <cberthold@technodotnet.com>
2016-03-02 21:31:53 -0500

    restructure for making event aggregate root

--- Security.BoundedContext/Domain/Feature/Feature.cs
+++ Security.BoundedContext/Domain/Feature/Feature.cs
@@ -1,4 +1,5 @@
-﻿using Infrastructure.Domain;
+﻿using CQRSlite.Domain;
+using Infrastructure.Domain;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,9 +8,8 @@ using System.Threading.Tasks;
 
 namespace Security.BoundedContext.Domain
 {
-    public class Feature : IEntity
+    public class Feature : AggregateRoot
     {
-        public Guid Id { get; protected set; }
         public string Name { get; protected set; }
         public bool IsEnabled { get; protected set; }
 
