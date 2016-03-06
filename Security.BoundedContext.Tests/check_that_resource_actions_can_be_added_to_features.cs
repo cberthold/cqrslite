d4e3ab71b90248b09bbbc1c4ab9b1f2a0939a956
Chris Berthold <cberthold@technodotnet.com>
2016-03-06 10:51:18 -0500

    finish out feature tests before refactoring api aggregate into domain service

--- Security.BoundedContext.Tests/check_that_resource_actions_can_be_added_to_features.cs
+++ Security.BoundedContext.Tests/check_that_resource_actions_can_be_added_to_features.cs
@@ -38,7 +38,7 @@ namespace Security.BoundedContext.Tests
             repository = null;
         }
 
-        
+
         private FeatureAggregate aggregate;
         private IRepository repository;
 
@@ -75,12 +75,13 @@ namespace Security.BoundedContext.Tests
 
             serviceAggregate = repository.Get<ApiAggregate>(ApiGuid);
         }
-        
+
 
         void TheFeatureIsCreated()
         {
             aggregate = FeatureAggregate.Create(FeatureName);
-            
+            FeatureId = aggregate.Id;
+
         }
 
         void TheResourceActionIsAddedToTheFeature()
@@ -101,27 +102,32 @@ namespace Security.BoundedContext.Tests
             Assert.IsNotNull(aggregate, "Feature couldnt be found in repository");
         }
 
-        void TheAggregateNameShouldEqualApiName()
+        void TheFeatureAggregateNameShouldEqualToFeatureName()
         {
             Assert.AreEqual(aggregate.Name, FeatureName);
         }
-        
 
-        //void AddingTheSameResourceActionShouldFailWithDomainError()
-        //{
-        //    var duplicateFound = false;
+        void TheLoadedResourceActionEntityIdsShouldBeFoundOnTheApiService()
+        {
+            Assert.IsTrue(aggregate.ResourceActions.Count > 0);
 
-        //    try
-        //    {
-        //        ResourceActionIsAdded();
-        //    }
-        //    catch (DomainException ex) when (ex.Message == "resource action already exists")
-        //    {
-        //        duplicateFound = true;
-        //    }
+            foreach (var addedResourceAction in aggregate.ResourceActions)
+            {
+                var serviceId = addedResourceAction.ApiServiceId;
+                var resourceActionId = addedResourceAction.ResourceActionEntityId;
 
-        //    Assert.IsTrue(duplicateFound, "duplicate resource action not found");
-        //}
+                var serviceAggregate = repository.Get<ApiAggregate>(serviceId);
+
+                Assert.IsNotNull(serviceAggregate, "Api service aggregate could not be found");
+
+                var resourceAction = serviceAggregate.FindResourceAction(resourceActionId);
+
+                Assert.IsNotNull(resourceAction, $"Unable to find the resource action with Id {resourceActionId}");
+                Assert.AreEqual(resourceAction.ResourceName, ResourceName);
+                Assert.AreEqual(resourceAction.ActionName, ActionName);
+
+            }
+        }
 
         [TestMethod]
         public void run_check_that_resource_actions_can_be_added_to_api_service()
@@ -131,7 +137,10 @@ namespace Security.BoundedContext.Tests
                 //.And(a => a.TheApiIsCreated())
                 .And(a => a.ResourceActionIsAddedToServiceApi())
                 .When(a => a.TheFeatureIsCreated())
-                .And(a=> a.TheResourceActionIsAddedToTheFeature())
+                .And(a => a.TheResourceActionIsAddedToTheFeature())
+                .And(a => a.TheFeatureAggregateIsSavedToTheRepository())
+                .And(a => a.TheFeatureShouldBeAbleToBeLoadedFromRepository())
+                .And(a => a.TheFeatureAggregateNameShouldEqualToFeatureName())
                 //.Then(a => a.TheFeatureShouldBeAbleToBeLoadedFromRepository())
                 //.And(a => a.TheAggregateNameShouldEqualApiName())
                 //.And(a => a.AddingTheSameResourceActionShouldFailWithDomainError())
